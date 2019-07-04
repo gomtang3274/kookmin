137744ef22c640f3c1a7c8a725bfc5e905fcd4e1
gomtang3274 <52527817+gomtang3274@users.noreply.github.com>
2019-07-04 17:46:13 +0900

    Add files via upload

---
+++ 3_19_tue.py
@@ -0,0 +1,87 @@
+#3월 19일 (화) 과학과소프트웨어적사고
+
+#who = input("Enter your name :")
+#print("Hi",who)
+#age = int(input("How old r u?"))
+#print(age+10)
+
+#print(len("안녕하세요."))
+
+#print("Hi\n" + "tody")
+#print("\"")
+
+#multiline = """\
+#Don\'t le the noise of other \'s opinions dron out your inner voice."""
+
+#print(multiline)
+
+#pi=3.14
+#x=3
+#y=4
+#print("pi is %d"%pi)
+#print("pi is %f"%pi)
+#print("pi is %e"%pi)
+#print("x is %d, y is %d"%(x,y))
+#print("Hello, %s"%input())
+
+#print("Error is %f%%" %3.234)
+#print("%o %x %x"%(9,10,255))
+
+#print("Error is {}%%".format(3.234))
+
+
+#print("안녕하세요"[0:2])
+#print("안녕하세요"[1:3])
+#print("안녕하세요"[2:4])
+#print("안녕하세요"[:3])
+#print("안녕하세요"[3:])
+
+
+#somebody=input:")
+
+#import random
+
+#i=1
+#print("Random #%d: %d" %(i, random.randint(1,45)) )
+#i+=1
+#print("Random #%d: %d" %(i, random.randint(1,45)) )
+#i+=1
+#print("Random #%d: %d" %(i, random.randint(1,45)) )
+#i+=1
+#print("Random #%d: %d" %(i, random.randint(1,45)) )
+#i+=1
+#print("Random #%d: %d" %(i, random.randint(1,45)) )
+
+
+#a=int(input())
+#b=int(input())
+#print("a+b: ",a+b)
+
+
+
+
+#from urllib.request import urlopen
+#f =urlopen('https://www.python.org/')
+#print(f.headers)
+#html = f.read()
+#print(html)
+
+#x=10
+#y=20
+#if x<y:
+#    print (x, "is less than", y)
+#if x>y:
+#    print(x, "is greaer than", y)
+#if x==y:
+#    print(x, "is equal to", y)
+
+#print(bool(3>2))
+
+#number = int(input("정수 입력>: "))
+#if number%2==1:
+#    print("{}는 홀수입니다.".format(number))
+#else :
+#    print("{}는 짝수입니다.".format(number))
+
+
+
